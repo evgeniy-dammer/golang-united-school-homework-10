5de8e4211765a0eedfab74f4ae682ffec5e0b099
Evgeniy Dammer <evgeniydammer@gmail.com>
2022-06-28 11:00:46 +0500

    Add solution
    Add solution

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -7,8 +7,16 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/GolangUnited/helloweb/cmd/handlers"
 	"github.com/gorilla/mux"
 )
+
+func AddHandlers(router *mux.Router) {
+	router.HandleFunc("/name/{PARAM}", handlers.NameParam).Methods(http.MethodGet)
+	router.HandleFunc("/bad", handlers.BadParam).Methods(http.MethodGet)
+	router.HandleFunc("/data", handlers.BodyParam).Methods(http.MethodPost)
+	router.HandleFunc("/headers", handlers.HeadersParam).Methods(http.MethodPost)
+}
 
 /**
 Please note Start functions is a placeholder for you to start your own solution.
@@ -21,7 +29,10 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
+	AddHandlers(router)
+
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
+
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
@@ -31,8 +42,10 @@ func Start(host string, port int) {
 func main() {
 	host := os.Getenv("HOST")
 	port, err := strconv.Atoi(os.Getenv("PORT"))
+
 	if err != nil {
 		port = 8081
 	}
+
 	Start(host, port)
 }
